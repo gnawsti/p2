7acaf4c07446c7f6cd19bc9aafe1c9575cefb039
gnawsti <awang0124@gmail.com>
2016-02-18 12:42:23 -0500

    Added separator option.

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 		Number of Words (max 9):<input type="number" value=" " name="words" min="1" max="9" step="1"><br>
 		<input type="checkbox" name="number" value="true"> Add a Number<br>
 		<input type="checkbox" name="symbol" value="true"> Add a Symbol<br>
+		Separator: <input type="radio" name="separator" value="-" checked> -
+		<input type="radio" name="separator" value="~"> ~
+		<input type="radio" name="separator" value="."> .
+		<input type="radio" name="separator" value=""> None <br>
 		<input type="submit" value="Get a password">
 	</form>
 </body>
