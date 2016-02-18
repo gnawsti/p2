6038fad41e94ec930a5492db1b476e38f42bf3c5
gnawsti <awang0124@gmail.com>
2016-02-18 12:34:51 -0500

    Added form

--- index.php
+++ index.php
@@ -4,6 +4,13 @@
 	<title>xkcd Password Generator</title>
 </head>
 <body>
+	<h1>xkcd Password Generator</h1>
 
+	<form method="POST" action="index.php">
+		Number of Words (max 9):<input type="number" value=" " name="words" min="1" max="9" step="1"><br>
+		<input type="checkbox" name="number" value="true"> Add a Number<br>
+		<input type="checkbox" name="symbol" value="true"> Add a Symbol<br>
+		<input type="submit" value="Get a password">
+	</form>
 </body>
 </html>
