bc3ada7109eebd18d73f5651627478d5f5faab1d
gnawsti <awang0124@gmail.com>
2016-02-18 21:44:13 -0500

    Updated missing charset and also blank value attribute.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
 	<title>xkcd Password Generator</title>
 	<?php require "controller.php"; ?>
 </head>
@@ -8,7 +9,7 @@
 	<h1>xkcd Password Generator</h1>
 
 	<form method="POST" action="index.php">
-		Number of Words (max 9):<input type="number" value=" " name="words" min="1" max="9" step="1"><br>
+		Number of Words (max 9):<input type="number" name="words" min="1" max="9" step="1"><br>
 		<input type="checkbox" name="number" value="true"> Add a Number<br>
 		<input type="checkbox" name="symbol" value="true"> Add a Symbol<br>
 		Separator: <input type="radio" name="separator" value="-" checked> - &nbsp;
